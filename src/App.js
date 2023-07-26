dc92a40826d01401b242280ac6e559ed215831dc
umadevi <umadevikacuro536@gmail.com>
2023-07-27 04:54:56 +0530

    ipl-code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [blogsList, setblogsList] = useState([]);
+  const [selectedBlog, setSelectedBlog] = useState(null);
+
+  useEffect(() => {
+    ftechBlogs()
+  })
+
+  const ftechBlogs = async () => {
+    const api = "https://apis.ccbp.in/ipl";
+    const response = await axios.get(api)
+    console.log(response.data.teams)
+    setblogsList(response.data.teams);
+  }
+
+  const onClickBlogDetails = async (blogsId) => {
+    const api = `https://apis.ccbp.in/ipl${blogsId}`
+    const response = await axios.get(api);
+    setSelectedBlog(response.data)
+  }
+  console.log(blogsList);
+  console.log(selectedBlog);
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 ">
+          <h1 className='heading'> IPL Dashboard</h1>
+        </div>
+        <div className="col-md-4">
+          {blogsList.map((blogs) =>
+            <div className="d-flex flex-row mb-2 blogs_container">
+              <img src={blogs.team_image_url} alt={blogs.title} className=' image_url' />
+            <p style={{marginLeft:"50px"}}>{blogs.name}</p>
+            </div>
+
+          )}
+        </div>
+        
+        </div>
+    </div>
+      
+  )
+}
 export default App;
